91eeafdad5cce4b938f884d8f17b095fe666caac
Leonid Kuzmin <lndkuzmin@gmail.com>
2014-07-25 10:23:57 +0300

    fixed game synchronization

--- app/game.js
+++ app/game.js
@@ -3,7 +3,7 @@ define(['phaser', 'lodash'], function (Phaser, _) {
   var cursors;
   var deathStar;
   var yoda;
-  var timestamp;
+  var prevUpdateTime;
   var steps = [];
   var inputs = [];
   var game = function (isServer, dataChannel) {
@@ -16,35 +16,27 @@ define(['phaser', 'lodash'], function (Phaser, _) {
       }
     });
     return {
-      preload: function () {
-        this.load.image('death_star', 'assets/death_star.gif');
-        this.load.image('yoda', 'assets/yoda.gif');
+      preload: function (game) {
+        game.load.image('death_star', 'assets/death_star.gif');
+        game.load.image('yoda', 'assets/yoda.gif');
       },
-      create: function () {
-        deathStar = this.add.sprite(0, 0, 'death_star');
-        yoda = this.add.sprite(this.world.width - 40, this.world.height - 36, 'yoda');
+      create: function (game) {
+        game.stage.disableVisibilityChange = true;
+        deathStar = game.add.sprite(0, 0, 'death_star');
+        yoda = game.add.sprite(game.world.width - 40, game.world.height - 36, 'yoda');
         deathStar.toJSON = function () {
           return _.pick(this, ['x', 'y']);
         };
         yoda.toJSON = function () {
           return _.pick(this, ['x', 'y']);
         };
-        cursors = this.input.keyboard.createCursorKeys();
-        timestamp = (new Date()).getTime();
-        if (isServer) {
-          setTimeout(function send() {
-            var currentTimestamp = (new Date()).getTime();
-            dataChannel.send(JSON.stringify({
-              type: 'step',
-              deathStar: deathStar,
-              yoda: yoda,
-              timestamp: currentTimestamp
-            }));
-            setTimeout(send, 45);
-          }, 45);
-        }
+        cursors = game.input.keyboard.createCursorKeys();
       },
-      update: function () {
+      update: function (game) {
+        if (_.isUndefined(prevUpdateTime)) {
+          prevUpdateTime = game.time.now;
+        }
+        var fromUpdate = game.time.now - prevUpdateTime;
         if (isServer) {
           if (cursors.left.isDown) {
             deathStar.x -= 5;
@@ -67,18 +59,39 @@ define(['phaser', 'lodash'], function (Phaser, _) {
             }
           }
           inputs = [];
+          if (fromUpdate >= 45) {
+            dataChannel.send(JSON.stringify({
+              type: 'step',
+              deathStar: deathStar,
+              yoda: yoda,
+              timestamp: game.time.now
+            }));
+            prevUpdateTime = game.time.now;
+          }
         } else {
+          var input = {
+            type: 'input'
+          };
+          if (cursors.left.isDown) {
+            input.key = 'left';
+          } else if (cursors.right.isDown) {
+            input.key = 'right';
+          } else if (cursors.up.isDown) {
+            input.key = 'up';
+          } else if (cursors.down.isDown) {
+            input.key = 'down';
+          }
+          dataChannel.send(JSON.stringify(input));
           if (steps.length >= 2) {
-            var currentTimestamp = (new Date()).getTime();
             var lastStep;
             for (var j = steps.length - 1; j >= 0; j--) {
-              if (steps[j].timestamp <= currentTimestamp) {
+              if (steps[j].timestamp <= game.time.now) {
                 lastStep = steps[j];
                 break;
               }
             }
             var firstStep = steps[j - 1];
-            var offsetTime = currentTimestamp - firstStep.timestamp;
+            var offsetTime = game.time.now - firstStep.timestamp;
             var t = offsetTime / (lastStep.timestamp - firstStep.timestamp);
             if (t > 1) {
               t = 1;
@@ -90,25 +103,9 @@ define(['phaser', 'lodash'], function (Phaser, _) {
             deathStar.y = firstStep.deathStar.y + (lastStep.deathStar.y - firstStep.deathStar.y) * t;
             yoda.x = firstStep.yoda.x + (lastStep.yoda.x - firstStep.yoda.x) * t;
             yoda.y = firstStep.yoda.y + (lastStep.yoda.y - firstStep.yoda.y) * t;
-            var input = {
-              type: 'input'
-            };
-            if (cursors.left.isDown) {
-              yoda.x -= 5;
-              input.key = 'left';
-            } else if (cursors.right.isDown) {
-              yoda.x += 5;
-              input.key = 'right';
-            } else if (cursors.up.isDown) {
-              yoda.y -= 5;
-              input.key = 'up';
-            } else if (cursors.down.isDown) {
-              yoda.y += 5;
-              input.key = 'down';
-            }
-            dataChannel.send(JSON.stringify(input));
           }
         }
+
       }
     };
   };
